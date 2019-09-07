b30f5bc697d9f51eb511fb959610a8b0e95a4b8b
Dan Milne <d@nmilne.com>
2019-09-07 17:38:18 +1000

    Move to using Amazon Signer, rather than reimplementing.

--- lib/aws_paa/request.rb
+++ lib/aws_paa/request.rb
@@ -1,42 +1,47 @@
-
+require 'byebug'
 
 module AwsPaa
   class Request
-    def initialize()
-      @request_example = {
-        "Keywords": "Harry",
-        "Marketplace": "www.amazon.com",
-        "PartnerTag": "xyz-20",
-        "PartnerType": "Associates",
-        "Resources": ["Images.Primary.Small","ItemInfo.Title","Offers.Listings.Price"],
-        "SearchIndex": "All"
-      }
+    include AwsRequest
+    attr_accessor :client, :marketplace, :timestamp, :datestamp, :amzstamp, :resources, :service
+    attr_accessor :headers, :payload
 
-      @headers = {
-        'host' => 'webservices.amazon.com',
-        'content-type' => 'application/json; charset=utf-8',
-        'content-encoding' => 'amz-1.0',
-        'x-amz-date' => '20160925T120000Z',
-        'x-amz-target' => 'com.amazon.paapi5.v1.ProductAdvertisingAPIv1.SearchItems',
-        'Authorization' => 'AWS4-HMAC-SHA256 Credential=AKIAIOSFODNN7EXAMPLE, SignedHeaders=content-type;host;x-amz-date;x-amz-target, Signature=&5d672d79c15b13162d9279b0855cfba6789a8edb4c82c400e06b5924a6f2b5d7;'
-      }
+    def initialize(client:)
+      @client      = client
+      @marketplace = client.marketplace
+      @partner_tag = client.partner_tag
+      @partner_type = 'Associates'
 
-      @marketplaces = [www.amazon.com]
+      @resources   = [
+        "Images.Primary.Large",
+        "ItemInfo.ContentInfo",
+        "ItemInfo.ProductInfo",
+        "ItemInfo.Title",
+        "ItemInfo.ExternalIds",
+      ]
+    end
+
+    def get_items(item_ids:,  **options)
+      item_ids = Array(item_ids)
+
+      payload = { ItemIds: item_ids, Resources:  @resources }
+
+      do_request(op: :get_items, payload: payload)
+    end
+
+    def get_variations(asin:)
+      payload = { ASIN: asin, Resources:  @resources }
+
+      do_request(op: :get_variations, payload: payload)
+    end
+
+    def search_items(keywords: )
+      payload = { Keywords: keywords, Resources:  @resources }
+
+      do_request(op: :search_items, payload: payload)
     end
 
     def get_browse_nodes
-      raise NotImplemented
-    end
-
-    def get_items
-      raise NotImplemented
-    end
-    
-    def get_variations
-      raise NotImplemented
-    end
-    
-    def search_items
       raise NotImplemented
     end
   end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,4 @@
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "aws_paa"
-
+require 'byebug'
 require "minitest/autorun"
