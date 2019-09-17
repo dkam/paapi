194403a974cfee8a536038decbe2f44fd3b8f36c
Dan Milne <d@nmilne.com>
2019-09-18 09:50:27 +1000

    Helper class

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,3 +2,10 @@ $LOAD_PATH.unshift File.expand_path('../lib', __dir__)
 require 'paapi'
 require 'byebug'
 require 'minitest/autorun'
+
+class MockHttpResponse
+  attr_accessor :body
+  def initialize(body)
+    @body = body
+  end
+end
