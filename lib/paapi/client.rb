747e76fe8f311ae384928d7c987063fabd2dbe75
Dan Milne <d@nmilne.com>
2020-08-21 17:58:56 +1000

    Require net/http and set the body to JSON

--- lib/paapi/client.rb
+++ lib/paapi/client.rb
@@ -1,3 +1,4 @@
+require 'net/http'
 require 'aws-sigv4'
 
 module Paapi
@@ -105,7 +106,7 @@ module Paapi
       request = Net::HTTP::Post.new(uri)
       request.content_type = 'application/json; charset=UTF-8'
       headers.each { |k, v| request[k] = v }
-      request.body = body
+      request.body = body.to_json
       req_options = { use_ssl: uri.scheme == 'https' }
       Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
         http.request(request)
